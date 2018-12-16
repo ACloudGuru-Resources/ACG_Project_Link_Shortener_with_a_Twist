5f45d117d42e10ff88c8d2aa49582c4deb531c0b
Rob Moore <me@robdmoore.id.au>
2018-12-17 00:44:56 +0800

    Added proxy file backup and SQL to add more links

---
+++ 08_addmorelinks.sql
@@ -0,0 +1,14 @@
+INSERT INTO Links (Id, ShortenedPath, RedirectURl, CreatedAt, CreatedBy)
+VALUES (newid(), 'microsoft', 'https://microsoft.com/', sysdatetimeoffset(), 'rob');
+
+INSERT INTO Links (Id, ShortenedPath, RedirectURl, CreatedAt, CreatedBy)
+VALUES (newid(), 'acg', 'https://acloud.guru/', sysdatetimeoffset(), 'rob');
+
+INSERT INTO Links (Id, ShortenedPath, RedirectURl, CreatedAt, CreatedBy)
+VALUES (newid(), 'facebook', 'https://facebook.com/', sysdatetimeoffset(), 'rob');
+
+INSERT INTO Links (Id, ShortenedPath, RedirectURl, CreatedAt, CreatedBy)
+VALUES (newid(), 'xkcd', 'https://xkcd.com/', sysdatetimeoffset(), 'rob');
+
+INSERT INTO Links (Id, ShortenedPath, RedirectURl, CreatedAt, CreatedBy)
+VALUES (newid(), 'apple', 'https://www.apple.com/', sysdatetimeoffset(), 'rob');
